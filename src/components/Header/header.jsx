7c91f84ca133d74196ceaab2fcf2e767c4d1d6ed
vaibhav9579 <nikucopa766@gmail.com>
2025-07-19 19:00:12 +0530

    logo

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -25,14 +25,13 @@ export const Header=()=>{
         window.scrollTo({ top: 850, behavior:"smooth" });
     }
 
-
-
     return(
-        <div className="main-header" >
+        <div className="main-header">
             <div className="header-content">
-                <h2>VIJAYA LAXMI FOOD COURT</h2>
+                <span style={{display:"flex", justifyContent:"center"}}><img src="/ast/lg.png"  width="300px" height="200px"/> </span>
+                <div className="heading">  <h2> VIJAYA LAXMI FOOD COURT</h2> </div>
                 <p>"Your Hunger, Our Priority - Let's Serve!"</p>
-                <p><span><img src="/ast/phone.svg" alt="" width="15px"/></span><span>9032785569 - </span> <span> 7013382707</span></p>
+                <p><span><img src="/ast/phone.svg" alt="" width="20px"/> </span><span><a href="tel:9032785569">9032785569</a> - </span> <span> <a href="tel:7013382707">7013382707</a></span></p>
                 {/* <button onClick={viewMenuclick}> View Menu </button> */}
             </div>
         </div>
